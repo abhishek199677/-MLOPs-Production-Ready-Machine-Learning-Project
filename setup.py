c739625948e98c7db76ce2fc738f4a58ae5f4f89
abhishek199677 <108387488+abhishek199677@users.noreply.github.com>
2024-11-27 13:41:15 +0530

    updates setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="abhishek199677",
+    author_email="puttapoguabhishek1007@gmail.com",
+    packages=find_packages()
+)
