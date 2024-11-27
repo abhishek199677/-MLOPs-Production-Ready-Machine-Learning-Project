0848efad253e393e0a66adce6d61747d690ca442
abhishek199677 <108387488+abhishek199677@users.noreply.github.com>
2024-11-27 13:50:51 +0530

    deleted

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="us_visa",
-    version="0.0.0",
-    author="abhishek199677",
-    author_email="puttapoguabhishek1007@gmail.com",
-    packages=find_packages()
-)
