aca91929eb0b21215636b5e7894d9292c5d756e9
abhishek199677 <108387488+abhishek199677@users.noreply.github.com>
2024-11-29 13:44:58 +0530

    experimental purpose

--- demo.py
+++ demo.py
@@ -7,4 +7,7 @@ logging.info("Welcome to logging")
 try:
     a = 2 / 0
 except Exception as e:
-    raise USvisaException(f"An error occurred: {e}", sys)  # Ensure USvisaException accepts these arguments
+    raise USvisaException(e, sys)   #Ensure USvisaException accepts these arguments
+
+
+    
